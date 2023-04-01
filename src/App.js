b5619334cf3007cf711da49eb75faf37fd438309
Ideja Ferati <idejaferati03@gmail.com>
2023-04-01 23:31:44 +0200

    Implemented client-side routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Articles from './pages/articles';
+import Discover from './pages/discover';
+import Profile from './pages/profile';
+import MyRecipes from './pages/myrecipes';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello !</h1>
-    </div>
-  );
+return (
+	<Router>
+		<Navbar />
+		<Routes>
+			<Route path='/' exact element={<Home/>}  />
+			<Route path='/about' element={<About/>} />
+			<Route path='/discover' element={<Discover/>} />
+			<Route path='/profile' element={<Profile/>} />
+			<Route path='/myrecipes' element={<MyRecipes/>} />
+			<Route path='/articles' element={<Articles/>} />
+		</Routes>
+	</Router>
+);
 }
 
 export default App;
