d02fea9968c059f513dfe80eae350f4f73b63ccb
Ideja Ferati <idejaferati03@gmail.com>
2023-04-27 22:26:57 +0200

    create shopping cart

---
+++ src/pages/shopping.js
@@ -0,0 +1,52 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+
+const Shopping = () => {
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
+  return (
+    <div style={{
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '100vh'
+	}}>
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div>
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+
+      {totalAmount > 0 ? (
+        <div>
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/checkout");
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
+    </div>
+  );
+};
+
+export default Shopping;
