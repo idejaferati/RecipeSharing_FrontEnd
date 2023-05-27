adbd4f237093c3659a17a7bd5e0ab25a264a4081
Ideja Ferati <idejaferati03@gmail.com>
2023-05-27 04:47:06 +0200

    signup test

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -10,7 +10,6 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { registerUser } from "./../service/user-requests.js";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
